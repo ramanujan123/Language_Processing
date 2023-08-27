72c2163f6e9cc3441bd6cba18a815358d8b80936
Mitul <mitulvardhan@iitbhilai.ac.in>
2023-08-27 22:33:59 +0530

    rules to generate tokens for function declaration and function invocation added

--- input.cpp
+++ input.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 // Hello World
 
+int sum(int a, int b){
+	return a+b;
+}
+
 int main(){
 	int a=9;
 	int b=10;
 	string c="Hello World";
+	cout<<sum(a,b)<<endl;
 	return 0;
 }
